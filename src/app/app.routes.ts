d226a9f0ac02ffe89754761d487086ee3be4a100
salma-mohamed-37 <lulymohamed37@gmail.com>
2024-07-19 13:58:56 +0300

    homepage , header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
   {
-    path : "",
-    loadChildren:()=> import("./weather/weather.module").then((m=>m.WeatherModule))
+    path:"",
+    component:HomePageComponent
+  },
+  {
+    path:"weather",
+    loadChildren : () => import ('./weather/weather.module').then((m)=>m.WeatherModule)
   }
+
 ];
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+ start () {
+  window.location.href = "weather"
+
+}
+}
